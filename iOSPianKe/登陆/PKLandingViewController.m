386acf8a66bf69db94e2521e031edb73cec9cf2a
xiaopeng12300 <424267351@qq.com>
2016-01-18 10:07:36 +0800

    1月18日，登陆页面添加屏幕上移动画

--- iOSPianKe/登陆/PKLandingViewController.m
+++ iOSPianKe/登陆/PKLandingViewController.m
@@ -9,9 +9,13 @@
 #import "PKLandingViewController.h"
 #import "PKThirdLandingView.h"//第三方登录view
 #import "PKEmailLandingView.h"//邮箱正常登陆
-@interface PKLandingViewController ()
+@interface PKLandingViewController ()<UITextFieldDelegate>
 @property (strong, nonatomic)PKThirdLandingView *thirdLandingView;
 @property (strong, nonatomic)           PKEmailLandingView *emailLandingView;
+@property (strong, nonatomic)           UIImageView *iconImage;
+@property (strong, nonatomic)           UIButton *returnBtn;
+@property (strong, nonatomic)           UIButton *loginBtn;
+
 @end
 
 @implementation PKLandingViewController
@@ -19,21 +23,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.view  addSubview:self.thirdLandingView];
-    WS(weakSelf);
-    [_thirdLandingView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.equalTo(weakSelf.view.mas_left);
-        make.right.equalTo(weakSelf.view.mas_right);
-        make.bottom.equalTo(weakSelf.view.mas_bottom);
-        make.height.equalTo(105);
-    }];
+    
     [self.view addSubview:self.emailLandingView];
+    
+    [self.view addSubview:self.loginBtn];
+    [self.view addSubview:self.returnBtn];
+    [self.view addSubview:self.iconImage];
+    [self addAutoLayoutIn];
+}
+
+- (void)addAutoLayoutIn{
+    WS(weakSelf);
     [_emailLandingView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.bottom.equalTo(weakSelf.thirdLandingView.mas_top).offset(-50);
         make.left.equalTo(weakSelf.view.mas_left);
         make.right.equalTo(weakSelf.view.mas_right);
         make.height.equalTo(170);
     }];
+    [_thirdLandingView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(weakSelf.view.mas_left);
+        make.right.equalTo(weakSelf.view.mas_right);
+        make.bottom.equalTo(weakSelf.view.mas_bottom);
+        make.height.equalTo(105);
+    }];
+    [_loginBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.size.equalTo(CGSizeMake(40, 20));
+        make.right.equalTo(weakSelf.view.mas_right).offset(-20);
+        make.top.equalTo(weakSelf.view.mas_top).offset(40);
+    }];
     
+    [_returnBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.size.equalTo(CGSizeMake(40, 20));
+        make.left.equalTo(weakSelf.view.mas_left).offset(20);
+        make.top.equalTo(weakSelf.view.mas_top).offset(40);
+    }];
+    
+    [_iconImage mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.size.equalTo(CGSizeMake(100, 50));
+        make.centerX.equalTo(weakSelf.view.mas_centerX);
+        make.centerY.equalTo(weakSelf.view.mas_centerY).offset(-100);
+    }];
 }
 
 //底部第三方登录view
@@ -46,9 +75,82 @@
 - (PKEmailLandingView *)emailLandingView{
     if (!_emailLandingView) {
         _emailLandingView = [[PKEmailLandingView alloc]init];
+        _emailLandingView.emailText.delegate = self;
+        _emailLandingView.passwordText.delegate = self;
     }
     return _emailLandingView;
 }
+
+//图标
+- (UIImageView *)iconImage{
+    if (!_iconImage ) {
+        _iconImage = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"片刻LOGO"]];
+    }
+    return _iconImage;
+}
+//注册
+- (UIButton *)loginBtn{
+    if (!_loginBtn) {
+        _loginBtn = [UIButton buttonWithType:(UIButtonTypeCustom)];
+        [_loginBtn setTitle:@"注册" forState:(UIControlStateNormal)];
+        [_loginBtn setTitleColor:[UIColor blackColor] forState:(UIControlStateNormal)];
+    }
+    return _loginBtn;
+}
+- (UIButton *)returnBtn{
+    if (!_returnBtn) {
+        _returnBtn = [UIButton buttonWithType:(UIButtonTypeCustom)];
+        [_returnBtn setImage:[UIImage imageNamed:@"返回"] forState:(UIControlStateNormal)];
+        [_returnBtn addTarget:self action:@selector(returnViewMethod) forControlEvents:(UIControlEventTouchUpInside)];
+    }
+    return _returnBtn;
+}
+
+- (void)returnViewMethod{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField{
+    WS(weakSelf);
+    if (textField == _emailLandingView.emailText) {
+        [UIView animateWithDuration:0.3 animations:^{
+            CGRect rect = weakSelf.view.bounds;
+            rect.origin.y = 50;
+            weakSelf.view.bounds = rect;
+        }];
+    }else{
+        [UIView animateWithDuration:0.3 animations:^{
+            CGRect rect = weakSelf.view.bounds;
+            rect.origin.y = 100;
+            weakSelf.view.bounds = rect;
+        }];
+    }
+    return YES;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [_emailLandingView.emailText resignFirstResponder];
+    [_emailLandingView.passwordText resignFirstResponder];
+    WS(weakSelf);
+    [UIView animateWithDuration:0.3 animations:^{
+        CGRect rect = weakSelf.view.bounds;
+        rect.origin.y = 0;
+        weakSelf.view.bounds = rect;
+    }];
+    return YES;
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [_emailLandingView.emailText resignFirstResponder];
+    [_emailLandingView.passwordText resignFirstResponder];
+    WS(weakSelf);
+    [UIView animateWithDuration:0.3 animations:^{
+        CGRect rect = weakSelf.view.bounds;
+        rect.origin.y = 0;
+        weakSelf.view.bounds = rect;
+    }];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
@@ -56,3 +158,7 @@
 
 
 @end
+
+
+
+
